d4494942e9a2e7491c8b100a31441c26253d7662
ThijsVanZon <thijs.vanzon@existenceinitiative.com>
2024-05-29 10:27:02 +0200

    existence.app 0.3.0

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const decades = document.querySelectorAll(".decade");
-  
-    decades.forEach((decade, index) => {
-      decade.addEventListener("mouseenter", () => {
-        highlightDecade(index);
-      });
-      decade.addEventListener("mouseleave", () => {
-        resetDecadeHighlighting();
-      });
+    const mainDecades = document.querySelectorAll(".main-decade-container");
+    const subDecades = document.querySelectorAll(".sub-decade-container");
+
+    mainDecades.forEach((decade, index) => {
+        decade.addEventListener("mouseenter", () => {
+            highlightDecade(index);
+        });
     });
-  
+
     function highlightDecade(index) {
-      if (index > 0) {
-        decades[index - 1].classList.add("highlight");
-      }
-      if (index < decades.length - 1) {
-        decades[index + 1].classList.add("highlight");
-      }
-      decades[index].classList.add("highlight");
+        resetDecadeHighlighting();
+        mainDecades[index].classList.add("highlight");
+
+        // Highlight the nearest two sub-decades
+        if (index > 0) {
+            subDecades[index].classList.add("highlight");
+        }
+        if (index < mainDecades.length - 1) {
+            subDecades[index + 1].classList.add("highlight");
+        } else {
+            // Edge case for the last main decade (2050)
+            subDecades[index + 1].classList.add("highlight");
+        }
+
+        // Special case for the first main decade (2000)
+        if (index === 0) {
+            subDecades[0].classList.add("highlight");
+        }
     }
-  
+
     function resetDecadeHighlighting() {
-      decades.forEach(decade => {
-        decade.classList.remove("highlight");
-      });
+        mainDecades.forEach(mainDecade => {
+            mainDecade.classList.remove("highlight");
+        });
+        subDecades.forEach(subDecade => {
+            subDecade.classList.remove("highlight");
+        });
     }
-  });
+});
