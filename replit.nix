a45cd3a7a0d67eba1658794d41a31127beb09d53
Thijs van Zon <157784463+ThijsVanZon@users.noreply.github.com>
2024-06-03 07:54:27 +0000

    replit.nix expansion

--- replit.nix
+++ replit.nix
@@ -1,7 +1,20 @@
-{pkgs}: {
+{ pkgs }: {
   deps = [
-    pkgs.gitFull
     pkgs.glibcLocales
-    pkgs.openssh_gssapi
+    pkgs.gitFull
+    pkgs.python38Full
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      # Needed for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+      # Needed for matplotlib
+      pkgs.xorg.libX11
+    ];
+    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+    LANG = "en_US.UTF-8";
+  };
 }
