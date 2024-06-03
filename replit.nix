b66b840b0a3c24a3c443994cca5684fefad2aa1d
Thijs van Zon <157784463+ThijsVanZon@users.noreply.github.com>
2024-06-03 08:30:01 +0000

    existence.app 0.11.0

--- replit.nix
+++ replit.nix
@@ -3,7 +3,15 @@
     pkgs.glibcLocales
     pkgs.gitFull
     pkgs.python38Full
+    (pkgs.python38Full.withPackages (ps: with ps; [
+      flask
+      scrapy
+      twisted
+      requests
+      # Add any additional packages here
+    ]))
   ];
+
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       # Needed for pandas / numpy
@@ -13,6 +21,15 @@
       pkgs.glib
       # Needed for matplotlib
       pkgs.xorg.libX11
+      # Flask dependencies
+      pkgs.openssl
+      pkgs.libffi
+      # Scrapy & Twisted dependencies
+      pkgs.libxml2
+      pkgs.libxslt
+      pkgs.libffi
+      pkgs.openssl
+      pkgs.zlib
     ];
     PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
     LANG = "en_US.UTF-8";
